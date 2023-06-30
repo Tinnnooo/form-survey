51e86928b16a8a3f648e4e05aa0da80ed0515923
Tinno <tinnofabregas01@gmail.com>
2023-06-30 21:09:15 +0700

    enum solve

--- database/migrations/2023_06_26_061200_create_questions_table.php
+++ database/migrations/2023_06_26_061200_create_questions_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->bigInteger('form_id');
             $table->string('name');
-            $table->enum('choice_type', ['short answers', 'paragraph', 'date', 'time', 'multiple choice', 'dropdown', 'checkboxes']);
+            $table->enum('choice_type', ['short answer', 'paragraph', 'date', 'time', 'multiple choice', 'dropdown', 'checkboxes']);
             $table->string('choices');
             $table->tinyInteger('is_required');
             $table->timestamps();
